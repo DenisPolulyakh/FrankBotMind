1c8c6a8c5d2b6387c2612805782f4bc5712f675c
rich.hcir.error@gmail.com <denis.polulyakh.java@gmail.com>
2017-01-06 02:19:06 +0400

    оооо

--- src/main/java/ru/dpolulyakh/www/datamodel/StockDaoImpl.java
+++ src/main/java/ru/dpolulyakh/www/datamodel/StockDaoImpl.java
@@ -4,6 +4,7 @@ import org.hibernate.FlushMode;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -21,6 +22,7 @@ public class StockDaoImpl extends HibernateDaoSupport implements StockDao {
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void save(Stock stock) {
         // Programmatic transaction management
         transactionTemplate.execute(new TransactionCallbackWithoutResult() {
