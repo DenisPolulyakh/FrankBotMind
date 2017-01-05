00f5040e6e0a0388e417da441ca71fb025523585
rich.hcir.error@gmail.com <denis.polulyakh.java@gmail.com>
2017-01-06 00:23:30 +0400

    @Transactional

--- src/main/java/ru/dpolulyakh/www/datamodel/StockDaoImpl.java
+++ src/main/java/ru/dpolulyakh/www/datamodel/StockDaoImpl.java
@@ -1,6 +1,7 @@
 package ru.dpolulyakh.www.datamodel;
 
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,18 +10,22 @@ import java.util.List;
  */
 public class StockDaoImpl extends HibernateDaoSupport implements StockDao{
     @Override
+    @Transactional
     public void save(Stock stock){
-        getHibernateTemplate().save(stock);
+                getHibernateTemplate().save(stock);
     }
     @Override
+    @Transactional
     public void update(Stock stock){
         getHibernateTemplate().update(stock);
     }
     @Override
+    @Transactional
     public void delete(Stock stock){
         getHibernateTemplate().delete(stock);
     }
     @Override
+    @Transactional
     public Stock findByStockCode(String stockCode){
         List list = getHibernateTemplate().find("from Stock where stockCode=?",stockCode);
         return (Stock)list.get(0);
